8fe75c5ab57ce977b30074549f3aef6e736e40e9
uladzislaupravalenak <vegas16777@gmail.com>
2023-04-25 00:18:35 +0400

    Remove unused module

--- shared/profile/src/main/kotlin/com/pracel/authorizationApp/shared/profile/validator/api/ProfileValidator.kt
+++
@@ -1,8 +0,0 @@
-package com.pracel.authorizationApp.shared.profile.validator.api
-
-interface ProfileValidator {
-
-    fun validateEmail(email: String): Boolean
-
-    fun validatePassword(password: String): Boolean
-}
