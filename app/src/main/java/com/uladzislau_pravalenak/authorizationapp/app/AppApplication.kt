6003231c04bb86742417cd3ee3674197d692bcf2
uladzislaupravalenak <vegas16777@gmail.com>
2023-05-14 13:14:40 +0400

    Made workaround with Accounts

--- app/src/main/java/com/uladzislau_pravalenak/authorizationapp/app/AppApplication.kt
+++ app/src/main/java/com/uladzislau_pravalenak/authorizationapp/app/AppApplication.kt
@@ -2,7 +2,13 @@ package com.uladzislau_pravalenak.authorizationapp.app
 
 import android.app.Application
 import com.pracel.authorizationapp.accounts.api.di.AccountComponentProvider
+import com.pracel.authorizationapp.accounts.api.di.AccountModule
+import com.pracel.authorizationapp.accounts.api.di.AccountsComponent
+import com.pracel.authorizationapp.accounts.api.di.AccountsUiModule
+import com.pracel.authorizationapp.accounts.impl.di.AccountComponentDependencies
 import com.pracel.authorizationapp.accounts.impl.di.AccountComponentProviderImpl
+import com.pracel.authorizationapp.accounts.impl.di.AccountModule
+import com.pracel.authorizationapp.accounts.impl.di.AccountsUiModule
 import com.pracel.authorizationapp.analytics.api.di.AnalyticsComponent
 import com.pracel.authorizationapp.analytics.api.di.AnalyticsComponentProvider
 import com.pracel.authorizationapp.analytics.api.di.AnalyticsUiModule
@@ -18,21 +24,21 @@ import kotlinx.coroutines.Dispatchers
 class AppApplication : Application(),
     TransactionsComponentProvider by TransactionsComponentProviderImpl(),
     HomeComponentProvider by HomeComponentProviderImpl(),
-    AccountComponentProvider by AccountComponentProviderImpl(),
+    AccountComponentProvider ,
     AnalyticsComponentProvider {
 
 //    //TODO: fix issue and use approach for Transactions and Home features
-//    override fun provideAccountComponent(): AccountsComponent {
-//        val dependencies = AccountComponentDependencies(Dispatchers)
-//
-////        return AccountsComponent(dependencies)
-//        val accountModule = AccountModule(dependencies)
-//        val accountsUiModule = AccountsUiModule()
-//
-//        return object : AccountsComponent,
-//            AccountsUiModule by accountsUiModule,
-//            AccountModule by accountModule {}
-//    }
+    override fun provideAccountComponent(): AccountsComponent {
+        val dependencies = AccountComponentDependencies(Dispatchers)
+
+//        return AccountsComponent(dependencies)
+        val accountModule = AccountModule(dependencies)
+        val accountsUiModule = AccountsUiModule()
+
+        return object : AccountsComponent,
+            AccountsUiModule by accountsUiModule,
+            AccountModule by accountModule {}
+    }
 
     override fun provideAnalyticsComponent(): AnalyticsComponent {
         val analyticsUiModule = AnalyticsUiModuleImpl()
